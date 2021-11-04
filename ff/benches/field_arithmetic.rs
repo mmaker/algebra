9c41090c78f81d0b8f5ab6800e4524c6e24b235f
Michele Orru <michele.orru@berkeley.edu>
2021-11-04 19:35:53 +0000

    Update benchmarks to have a more similar behaviour.

--- ff/benches/field_arithmetic.rs
+++ ff/benches/field_arithmetic.rs
@@ -1,40 +1,50 @@
 #[macro_use]
 extern crate criterion;
 
-use ark_ff::Zero;
 use ark_ff::fields::Field;
 use ark_std::{
     ops::{AddAssign, MulAssign},
     UniformRand,
 };
-use ark_test_curves::bls12_381::Fr;
-use criterion::{black_box, Criterion};
+use ark_test_curves::bls12_381::Fr as F;
+use criterion::Criterion;
 
 fn bench_mul_add(c: &mut Criterion) {
     let rng = &mut ark_std::test_rng();
-    let mut a = Fr::rand(rng);
-    let multiplicand = Fr::rand(rng);
-    let addend = Fr::rand(rng);
 
-    assert_eq!(a * multiplicand + addend, a.mul_add(&multiplicand, &addend));
     let mut group = c.benchmark_group("ark_ff::mul_add");
     group.bench_function("mul+add_assign", |b| {
+        let mut a = F::rand(rng);
+        let multiplicand = F::rand(rng);
+        let addend = F::rand(rng);
         b.iter(|| {
-            a.mul_assign(multiplicand);
-            a.add_assign(addend);
-            assert_ne!(a, Fr::zero())
+            a.mul_assign(&multiplicand);
+            a.add_assign(&addend);
         })
     });
     group.bench_function("raw", |b| {
+        let mut a = F::rand(rng);
+        let multiplicand = F::rand(rng);
+        let addend = F::rand(rng);
         b.iter(|| {
-            let result = a * multiplicand + addend;
-            assert_ne!(result, Fr::zero())
+            a = a * &multiplicand + &addend;
+        })
+    });
+    group.bench_function("slow_raw", |b| {
+        let mut a = F::rand(rng);
+        let multiplicand = F::rand(rng);
+        let addend = F::rand(rng);
+        b.iter(|| {
+            a = a * &multiplicand;
+            a = a + &addend;
         })
     });
     group.bench_function("mul_add", |b| {
+        let mut a = F::rand(rng);
+        let multiplicand = F::rand(rng);
+        let addend = F::rand(rng);
         b.iter(|| {
             a.mul_add_assign(&multiplicand, &addend);
-            assert_ne!(a, Fr::zero())
         })
     });
 }
